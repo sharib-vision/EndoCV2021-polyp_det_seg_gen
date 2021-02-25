76fcb2e389d59bf78404b36f7b73e22aff375706
sharibox <sharib.ali@eng.ox.ac.uk>
2021-02-25 23:37:32 +0000

    small

--- voc2jsonCOCO.py
+++ voc2jsonCOCO.py
@@ -72,7 +72,7 @@ def images_annotations_info(args):
     root_path = args.root_path 
     dataset = {'categories': [], 'annotations': [], 'images': []}
      
-    with open(os.path.join(root_path, 'obj.names')) as f:
+    with open( 'obj.names') as f:
         classes = f.read().strip().split()
         
     for i, cls in enumerate(classes, 1):
@@ -96,9 +96,10 @@ def images_annotations_info(args):
         file_id = os.path.basename(os.path.normpath(file_id))
         lines_list = EndoCV_misc.file_lines_to_list(txt_file)
         
-        im = cv2.imread(os.path.join(root_path, 'images/') + file_id+'.jpg')
+        # im = cv2.imread(os.path.join(root_path, 'images/') + file_id+'.jpg')
+        im = cv2.imread(root_path + '/' +file_id+'.jpg')
         height, width, _ = im.shape
-        images.append(create_image_annotation(os.path.join(root_path, 'images/') + file_id+'.jpg', width, height, count))
+        images.append(create_image_annotation(os.path.join(root_path, '/') + file_id+'.jpg', width, height, count))
         
         for line in lines_list:
             try:
@@ -107,14 +108,22 @@ def images_annotations_info(args):
                     cls_id, x1, y1, x2, y2 = line.split()
                 else:
                     cls_id, score, x1, y1, x2, y2 = line.split()
-                    
+   
                 
                 width_box = max(0, float(x2) - float(x1))
                 height_box = max(0, float(y2)- float(y1))
                 
             except ValueError:
-                error_msg = "Error: File " + txt_file + " in the wrong format.\n"
-                EndoCV_misc.error(error_msg)
+                # error_msg = "Error: File " + txt_file + " in the wrong format.\n"
+                # EndoCV_misc.error(error_msg)
+                if args.type == 'GT':
+                    cls_id, x1, y1, x2, y2 = ('polyp', '-1', '-1', '-1', '-1')
+                else: 
+                    cls_id, x1, y1, x2, y2 = ('polyp', '-1', '-1', '-1', '-1')
+                    score = 0.9
+                
+                width_box = max(0, float(x2) - float(x1))
+                height_box = max(0, float(y2)- float(y1))
                 
             annotations.append(create_annotation_coco_format(float(x1), float(y1), width_box, height_box, score, count, 1, annot_count, args))
             
@@ -124,10 +133,18 @@ def images_annotations_info(args):
  
     
  
+# def get_args():
+#     parser = argparse.ArgumentParser('VOC format annotations to COCO dataset format')
+#     parser.add_argument('--root_path', default='/media/sharib/development/EndoCV2021-test_analysis/endocv2021-test-noCopyAllowed-v1/EndoCV_DATA1', type=str, help='Absolute path for \'train.txt\' or \'test.txt\'')
+#     parser.add_argument('--txtFiles_path', default='/media/sharib/development/EndoCV2021-test_analysis/codes-det/EndoCV2021/detection/EndoCV_DATA1_pred', type=str, help='Absolute')
+#     parser.add_argument('--type', default='pred', type=str, help='Name the output json file')
+#     args = parser.parse_args()
+#     return args
+
 def get_args():
-    parser = argparse.ArgumentParser('Yolo format annotations to COCO dataset format')
-    parser.add_argument('--root_path', default='/Volumes/myPC/EndoCV2021/EndoCV2021-polyp_det_seg_gen/example/', type=str, help='Absolute path for \'train.txt\' or \'test.txt\'')
-    parser.add_argument('--txtFiles_path', default='/Volumes/myPC/EndoCV2021/EndoCV2021-polyp_det_seg_gen/example/GT/', type=str, help='Absolute')
+    parser = argparse.ArgumentParser('VOC format annotations to COCO dataset format')
+    parser.add_argument('--root_path', default='/Volumes/myPC/EndoCV2021-test_analysis/endocv2021-test-noCopyAllowed-v1/EndoCV_DATA2', type=str, help='Absolute path for \'train.txt\' or \'test.txt\'')
+    parser.add_argument('--txtFiles_path', default='/Volumes/myPC/EndoCV2021-test_analysis/codes-det/EndoCV2021/detection/EndoCV_DATA2_pred', type=str, help='Absolute')
     parser.add_argument('--type', default='GT', type=str, help='Name the output json file')
     args = parser.parse_args()
     return args
@@ -143,15 +160,17 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    phase = 'train'
+    phase = 'EndoCV_DATA2_GT'
     classes = ['polyp']
  
-    folder = os.path.join(args.root_path, 'annotations')
+    # folder = os.path.join(args.root_path, 'annotations')
+    folder =  'annotations'
     if not os.path.exists(folder):
       os.makedirs(folder)
     
     coco_format['images'], coco_format['annotations'] = images_annotations_info(args)
-    json_name = os.path.join(args.root_path, 'annotations/{}.json'.format(phase))
+    
+    json_name = os.path.join('annotations/{}.json'.format(phase))
     
     for index, label in enumerate(classes):
         ann = {
